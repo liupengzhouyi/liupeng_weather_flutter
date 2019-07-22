3e49160c089bbf45239aa468f5038ded55c99a9f
liupeng <37412816+liupengzhouyi@users.noreply.github.com>
2019-07-22 21:12:59 +0800

    重新构建readme文件 V 1.1.0

---
+++ lib/weather.dart
@@ -0,0 +1,18 @@
+class Weather{
+  Weather();
+  String cityName;//城市名字
+  String updateTime;//更新时间
+  List<DataBean> dataBean = new List();//天气数据
+}
+class DataBean{
+  DataBean();
+  String day;//日期
+  String week;//周期
+  String wea;//天气
+  int air;//空气纯净指数
+  String airLevel;//空气纯净度
+  String airTips;
+  String tem;//当前温度
+  String tem1;//最高温度
+  String tem2;//最低温度
+}
